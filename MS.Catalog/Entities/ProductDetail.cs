feed027c36c4fc2567b284d6202c491560b973e1
Cavit Selman <tetikci462@hotmail.com>
2024-11-04 00:58:07 +0300

    Entities added.

---
+++ MS.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace MS.Catalog.Entities
+{
+    public class ProductDetail
+    {
+        public string ProductDetailID { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+    }
+}
