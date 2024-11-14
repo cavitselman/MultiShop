a48257330f4643906be52ed96d21bf38b8e88556
Cavit Selman <tetikci462@hotmail.com>
2024-11-14 16:17:19 +0300

    Writing the Discount Microservice, NuGet Packages, Migrations, DTOs, Services, and API

---
+++ Services/Discount/MS.Discount/Context/DapperContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MS.Discount.Entities;
+using System.Data;
+
+namespace MS.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;initial Catalog=MultiShopDiscountDb;integrated Security=true;Trusted_Connection=true;TrustServerCertificate=true");
+        }
+        public DbSet<Coupon> Coupones { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
---
+++ Services/Discount/MS.Discount/Services/IDiscountService.cs
@@ -0,0 +1,13 @@
+﻿using MS.Discount.Dtos;
+
+namespace MS.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<List<ResultDiscountCouponDto>> GetAllDiscountCouponAsync();
+        Task CreateDiscountCouponAsync(CreateDiscountCouponDto createCouponDto);
+        Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto);
+        Task DeleteDiscountCouponAsync(int id);
+        Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int id);
+    }
+}
