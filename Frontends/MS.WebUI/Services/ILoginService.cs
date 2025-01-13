e1a748d177cbd7e02e420ef317757ce22b359621
cavitselman <tetikci462@hotmail.com>
2025-01-13 17:55:01 +0300

    Manual token transactions.

---
+++ Frontends/MS.WebUI/Services/ILoginService.cs
@@ -0,0 +1,7 @@
+﻿namespace MS.WebUI.Services
+{
+    public interface ILoginService
+    {
+        public string GetUserId { get; }
+    }
+}
--- IdentityServer/MS.IdentityServer/Controllers/LoginsController.cs
+++ IdentityServer/MS.IdentityServer/Controllers/LoginsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MS.IdentityServer.Dtos;
 using MS.IdentityServer.Models;
+using MS.IdentityServer.Tools;
 
 namespace MS.IdentityServer.Controllers
 {
@@ -13,19 +14,27 @@ namespace MS.IdentityServer.Controllers
     public class LoginsController : ControllerBase
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public LoginsController(SignInManager<ApplicationUser> signInManager)
+        public LoginsController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [HttpPost]
         public async Task<IActionResult> UserLogin(UserLoginDto userLoginDto)
         {
             var result = await _signInManager.PasswordSignInAsync(userLoginDto.Username, userLoginDto.Password, false, false);
+            var user=await _userManager.FindByNameAsync(userLoginDto.Username);
+
             if (result.Succeeded)
             {
-                return Ok("Giriş Başarılı");
+                GetCheckAppUserViewModel model = new GetCheckAppUserViewModel();
+                model.Username = userLoginDto.Username;
+                model.Id = user.Id;
+                var token = JwtTokenGenerator.GenerateToken(model);
+                return Ok(token);
             }
             else
             {
