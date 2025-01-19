4d6f82e1371d1a6b3645b4eb693d28fcc89209bb
cavitselman <tetikci462@hotmail.com>
2025-01-19 16:14:22 +0300

    Service Api settings.

--- IdentityServer/MS.IdentityServer/Config.cs
+++ IdentityServer/MS.IdentityServer/Config.cs
@@ -99,7 +99,11 @@ public static class Config
                 ClientName="Multi Shop Manager User",
                 AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
                 ClientSecrets={new Secret("multishopsecret".Sha256()) },
-                AllowedScopes={ "CatalogReadPermission", "CatalogFullPermission", "BasketFullPermission", "CommentFullPermission", "PaymentFullPermission", "ImageFullPermission", "OcelotFullPermission" }
+                AllowedScopes={ "CatalogReadPermission", "CatalogFullPermission", "BasketFullPermission", "CommentFullPermission", "PaymentFullPermission", "ImageFullPermission", "OcelotFullPermission",
+                    IdentityServerConstants.LocalApi.ScopeName,
+                    IdentityServerConstants.StandardScopes.Email,
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile }
             },
 
             //Admin
