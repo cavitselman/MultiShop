6b4edf0c25f16194175dd8549064fd15cdd742d3
cavitselman <tetikci462@hotmail.com>
2025-01-31 22:36:43 +0300

    Credit card script.

---
+++ Frontends/MS.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MS.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
