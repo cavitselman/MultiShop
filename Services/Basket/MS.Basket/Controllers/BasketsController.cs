7dc1ab141774b574fd60bbac0b9b41a374e30fbe
cavitselman <tetikci462@hotmail.com>
2025-05-10 18:33:46 +0300

    Adding and saving products to the basket has been revised.

--- Services/Basket/MS.Basket/Controllers/BasketsController.cs
+++ Services/Basket/MS.Basket/Controllers/BasketsController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MS.Basket.Dtos;
 using MS.Basket.LoginServices;
 using MS.Basket.Services;
 
 namespace MS.Basket.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class BasketsController : ControllerBase
